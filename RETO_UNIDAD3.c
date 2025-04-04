ec813f23db966a2623ed772be990718e666f34b8
lauramartinez444 <lalomar1118@gmail.com>
2025-04-04 01:38:48 +0000

    finalicé el trabajo correspondiente al reto de la unidad 3

--- RETO_UNIDAD3.c
+++ RETO_UNIDAD3.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+#include <math.h>
 int menu(void);
 int main(){
-    menu();
+float factor_de_potencia(float angulo);
+float potencia_aparente(float potencia_activa, float factor_potencia);
+float potencia_activa(float potencia_aparente, float factor_potencia);
     return 0
 }
     
@@ -13,27 +15,79 @@ int main(int argc, char const *argv[])
     int opcion;
     encabezado();
     opcion = menu();
+
+    float factor_de_potencia(float angulo) {
+        float angulo_rad = angulo * (M_PI / 180.0); 
+        return cos(angulo_rad); 
+    }
+    
+    float potencia_aparente(float potencia_activa, float factor_potencia) {
+        return potencia_activa / factor_potencia;
+    }
+    
+    float potencia_activa(float potencia_aparente, float factor_potencia) {
+        return potencia_aparente * factor_potencia;
+    }
     return 0;
 }
 
 
-switch 
 {   
-    int valor de voltaje, valor de corriente, valor de resistencia, resistividad del material conductor, 
+    int valor_de_voltaje, valor_de_corriente, valor_de_resistencia, resistividad_del material_conductor, 
 
     printf("1. Ley de OHM\n");
     printf("2. Cálculo del Factor de potencia\n");
     printf("3. Resistencia de un Conductor\n");
     printf("4. Resistencia para LED\n");
-
     scanf("%d", &opcion);
+
+
     switch(opcion)
+    float valor_de_voltaje, valor_de_corriente, valor_de_resistencia, resistividad_del material_conductor, 
     {
-        case 1: printf("Ingrese que desea calcular\n \
-            1. Voltaje \n \
-            2. Corrinte \n \ 
-            3. Resistencia\n");
+        case 1: printf("Ingrese que desea calcular: \n") 
+                printf("1. Voltaje \n")
+                printf("2. Corriente \n") 
+                printf("3. Resistencia\n")
             scanf("%d", &opcion);
             switch(opcion)
-                case 1: printf("ingrese el valor de Corriente\n")
+            {
+                case 1: printf("ingrese el valor de Corriente: \n");
+                        scanf("%f", &valor_de_corriente);
+                        printf("ingrese el valor de la resistencia: \n");
+                        scanf("%f", &valor_de_resistencia);
+
+                        (valor_de_voltaje= valor_de_corriente * valor_de_resistencia); 
+
+                        printf("Valor de voltaje: %d \n", valor_de_voltaje)
+
+                case 2: printf("ingrese el valor de voltaje_: \n");
+                        scanf("%f", &valor_de_voltaje);
+                        printf("ingrese el valor de la resistencia\n");
+                        scanf("%f", &valor_de_resistencia);
+
+                        valor_de_corriente = valor_de_voltaje/valor_de_resistencia
+
+                        printf("Valor de corriente: %d \n", valor_de_corriente); 
+
+                case 3: printf("ingrese el valor de voltaje\n");
+                        scanf("%f", &valor_de_voltaje);
+                        printf("ingrese el valor de Corriente\n");
+                        scanf("%f", &valor_de_corriente);
+
+                        valor_de_resistencia = valor_de_voltaje/valor_de_corriente
+
+                        printf("Valor de resistencia: %d \n", valor_de_resistencia); 
+            
+                    break;
+           }
+        case 2: printf("ingrese que dato desea calcular: \n")
+        printf("1. Factor de potencia \n")
+        printf("2. Potencia activa \n") 
+        printf("3. Potencia aparente \n")
+        scanf("%d", &opcion);
+        switch(opcion)
+
+           break;
     }
+}
